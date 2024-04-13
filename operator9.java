f0f90b9b082ce7fba7aa52c8495ecc367b9823a0
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 18:20:44 +0530

    Loops

--- operator9.java
+++ operator9.java
@@ -3,7 +3,7 @@ package Basic_java_part_3;
 public class operator9 {
     public static void main(String[] args) {
     
-        for (int i = 0; i >= 5; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("*");
         }
     }
