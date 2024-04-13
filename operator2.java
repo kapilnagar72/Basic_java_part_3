23e687cab0e745e909ea88b03898590326bdf232
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 15:18:17 +0530

    ++a

---
+++ operator2.java
@@ -0,0 +1,28 @@
+package Basic_java_part_3;
+
+public class operator2 {
+    public static void main(String[] args) {
+    // int a = 5;
+    // a++;
+    // System.out.println(a);  //post increment
+
+    // int b = 5;
+    // a--;
+    // System.out.println(a);
+
+    int c = 5; // pre increment
+    ++c;
+    System.out.println(c);
+
+    int d = 5;
+    --d;
+    System.out.println(d);
+   
+    // more operation
+    int a = 5;
+    int b;
+    b = a++ + ++a + a-- ;
+    System.out.println(a);
+
+    }
+}
