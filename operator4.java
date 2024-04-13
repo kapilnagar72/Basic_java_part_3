4e011a498b42303442c3e886a949132f50e3e6e6
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 16:03:57 +0530

    Relational Operator

---
+++ operator4.java
@@ -0,0 +1,50 @@
+package Basic_java_part_3;
+
+public class operator4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        //     System.out.println(b == c);  // 1 type used direct print 
+        //     System.out.println(c==b);  
+
+           boolean result = a == b;
+           System.out.println(result);      // operator are used boolean result try use the boolean 
+
+        //   boolean result = a >= b;
+        //   System.out.println(result);
+
+        //   boolean result = a <= b;
+        //   System.out.println(result);
+
+        //   boolean result = a == c;
+        //   System.out.println(result);
+
+        //   boolean result = a >= c;
+        //   System.out.println(result);
+
+        //   boolean result = a <= c;
+        //   System.out.println(result);
+
+        //   boolean result = c == b;
+        //   System.out.println(result);
+
+        //   boolean result = a >= c;
+        //   System.out.println(result);
+
+        //   boolean result = c <= b;
+        //   System.out.println(result);
+
+        //   boolean result = c == a;
+        //   System.out.println(result);
+
+        //   boolean result = c == b;
+        //   System.out.println(result);
+        //   
+        //           
+        //   boolean result = b == c;
+        //   System.out.println(result);
+
+    }
+}
