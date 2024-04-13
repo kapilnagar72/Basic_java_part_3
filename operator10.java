7cdbdca654a79c1cb63e8d1b2559f2acdedb6f0b
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 18:36:18 +0530

    loops

---
+++ operator10.java
@@ -0,0 +1,20 @@
+package Basic_java_part_3;
+
+public class operator10 {
+    public static void main(String[] args) {
+
+        // whille loops
+        //    int i = 0;
+        //     while (i<5) {
+        //         System.out.println("*");
+        //         i++;
+        //     }
+
+        // do while  loops
+        int i = 0;
+        do{
+            System.out.println("*");
+            i++;
+        } while (i > 5);
+    }
+}
