5199778c7e9458b3b4a8c3ff7077034b5bed6090
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 18:54:11 +0530

    pattern

---
+++ operator11.java
@@ -0,0 +1,12 @@
+package Basic_java_part_3;
+
+public class operator11 {
+    public static void main(String[] args) {
+        
+        for(int i=0; i<4; i++){
+            for(int j=0; j<5; j++){
+                System.out.print("*");
+            } System.out.println();
+        }
+    }
+}
