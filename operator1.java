3bfe37e830251b0e2f882f95afbe17435bcd4d82
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 14:49:31 +0530

    ...

---
+++ operator1.java
@@ -0,0 +1,16 @@
+package Basic_java_part_3;
+
+public class operator1 {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        System.out.println(num1 + num2);  //30
+        System.out.println(num1 - num2);  //10
+        System.out.println(num1 * num2); //300
+        System.out.println(num1 / num2);  //2
+        System.out.println(num1 % num2); //0
+        int a = 15;
+        int b = 13;
+        System.out.println(a % b);   // 2
+    }
+}
