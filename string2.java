1e79bf4e3996a51459a68c7215055514d660b7db
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-14 14:56:28 +0530

    String

---
+++ string2.java
@@ -0,0 +1,15 @@
+package Basic_java_part_3;
+
+public class string2 {
+    public static void main(String[] args) {
+
+    //    String d1 = "code";
+    //    String d2 = "code"; 
+    //    System.out.println(s1==2);   
+   
+        String s1 = new String("code");
+        String s2 = new String("code");  
+
+        System.out.println(s1==s2);
+    }
+}
