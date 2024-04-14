18a976c15bae37a4065f114bccf7baebfabfb690
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-14 12:11:40 +0530

    2D Jagged Array

---
+++ array4.java
@@ -0,0 +1,17 @@
+package Basic_java_part_3;
+
+public class array4 {
+    public static void main(String[] args) {
+        String num[][] = {{"ram","virat","rohit","ankit"},
+                           {"vaibhav","vanii","veeru"},
+                           {"akaay","sem"},
+                           {"vedu"},
+                           {"kalpu","kiwi","jems"}};
+
+                           for(int i=0; i<=4; i++){
+                            for(int j=0; j<num[i].length;j++){
+                                System.out.print(num[i][j] +" ");
+                            }System.out.println();
+                           }
+    }
+}
---
+++ num.java
@@ -0,0 +1,5 @@
+package Basic_java_part_3;
+
+public class num {
+
+}
