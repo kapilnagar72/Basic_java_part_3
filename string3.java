622d4abd67ae41ec2152bee090a4538922789174
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-14 19:38:00 +0530

    compare string

---
+++ string3.java
@@ -0,0 +1,13 @@
+package Basic_java_part_3;
+
+public class string3 {
+    public static void main(String[] args) {
+        String s1 = "java8";
+        String s2 = "java8";
+        String s3 = "JaVa8";
+        System.out.println(s1 == s2);
+        System.out.println(s1==s3);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+    }
+}
