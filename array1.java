9e64467c0c4a5ddeedfd444ee8deb2569ae77381
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-14 11:26:19 +0530

    Arrays

---
+++ array1.java
@@ -0,0 +1,14 @@
+package Basic_java_part_3;
+
+public class array1 {
+    public static void main(String[] args) {
+        int num[] = { 2, 3, 4, 6, 1 };
+        //System.out.println(num[0]);
+        System.out.println(num.length);
+        
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(num[i]);
+        }
+
+    }
+}
---
+++ array2.java
@@ -0,0 +1,11 @@
+package Basic_java_part_3;
+
+public class array2 {
+    public static void main(String[] args) {
+        String[] a = { "kalpu", "kapil", "jons", "modi", "yogi" };
+
+        for(int i=0; i<=4; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
