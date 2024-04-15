4bbf5b611cadc1a1d817b6d6264b7377a02d6868
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-15 10:10:35 +0530

    string concat and more about string

---
+++ string4.java
@@ -0,0 +1,9 @@
+package Basic_java_part_3;
+
+public class string4 {
+    public static void main(String[] args) {
+        String s1 = "dev";
+        s1 = s1.concat("java");
+        System.out.println(s1);
+    }
+}
---
+++ string5.java
@@ -0,0 +1,16 @@
+package Basic_java_part_3;
+
+public class string5 {
+    public static void main(String[] args) {
+        String str = " i love india";
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.length());
+        System.out.println(str.charAt(12));
+        System.out.println(str.substring(1, 7));
+        System.out.println(str.lastIndexOf(str));
+
+
+    }
+}
