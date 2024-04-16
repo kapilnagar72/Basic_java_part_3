33c84e6a8c12d83f97b9e6195fe49994fa9dbad0
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-16 14:27:48 +0530

    multilevel to multilevel inhertance

---
+++ inher4.java
@@ -0,0 +1,28 @@
+package Basic_java_part_3;
+
+class animal { // parent class
+    void sleep() {
+        System.out.println("animal needs sleeps");
+    }
+}
+
+class tiger extends animal { // child class of animal
+
+}
+
+// child class of tiger
+class dog extends tiger {
+
+}
+
+// child class of dog
+class cat extends dog {
+
+}
+
+public class inher4 {
+    public static void main(String[] args) {
+        cat t = new cat(); // object
+        t.sleep();
+    }
+}
